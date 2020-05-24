874faef25e4ad2c209c029e46efd246660719e35
PauMAVA <paumachetti@gmail.com>
2020-05-24 13:32:12 +0200

    Added logo command

--- src/main/java/me/PauMAVA/SecurityWorld3/telegram/TelegramBotCore.java
+++ src/main/java/me/PauMAVA/SecurityWorld3/telegram/TelegramBotCore.java
@@ -3,8 +3,14 @@ package me.PauMAVA.SecurityWorld3.telegram;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendDocument;
 import com.pengrad.telegrambot.request.SendMessage;
 import me.PauMAVA.SecurityWorld3.mail.NewsletterHandler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class TelegramBotCore {
 
@@ -33,6 +39,25 @@ public class TelegramBotCore {
     void sendMessage(long chatId, String message) {
         SendMessage sendMessage = new SendMessage(chatId, message);
         telegramBot.execute(sendMessage);
+    }
+
+    void sendImageAsFile(long chatId, String fileName) {
+        SendDocument sendDocument = new SendDocument(chatId, new File(fileName));
+        telegramBot.execute(sendDocument);
+    }
+
+    private byte[] toByteArray(InputStream in) {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            while((in.read(buffer)) != -1) {
+                bos.write(buffer);
+            }
+            return bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new byte[0];
+        }
     }
 
     NewsletterHandler getNewsletterHandler() {
--- src/main/java/me/PauMAVA/SecurityWorld3/telegram/UpdateHandler.java
+++ src/main/java/me/PauMAVA/SecurityWorld3/telegram/UpdateHandler.java
@@ -14,12 +14,16 @@ public class UpdateHandler {
         String text = update.message().text();
         long chatId = update.message().chat().id();
         System.out.println("Recieved update: chatId={" + chatId + "},text={" + text + "}");
-        if (telegramBotCore.getNewsletterHandler().isWaitingForEmail(chatId)) {
+        if (text == null || text.isEmpty() || text.isBlank()) {
+            return;
+        } else if (telegramBotCore.getNewsletterHandler().isWaitingForEmail(chatId)) {
             telegramBotCore.getNewsletterHandler().parseEmail(text);
             telegramBotCore.getNewsletterHandler().removeFromWaitingForEmail(chatId);
         } else if (text.equalsIgnoreCase("/newsletter")) {
             telegramBotCore.sendMessage(chatId, "SW50cm9kdWNlIHlvdXIgZW1haWw6");
             telegramBotCore.getNewsletterHandler().addToWaitingForEmail(chatId);
+        } else if (text.equalsIgnoreCase("/logo")) {
+            telegramBotCore.sendImageAsFile(chatId, "src/main/resources/logo.jpg");
         }
     }
 
